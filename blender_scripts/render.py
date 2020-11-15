bc7f54d6a48ea9c92b7f07d4a398e54bc7824306
Neelim Novo <neelim98@gmail.com>
2020-11-15 11:59:48 -0800

    Added two wide-angle cameras to outposts. Setup multi-camera rendering

--- blender_scripts/render.py
+++ blender_scripts/render.py
@@ -1,7 +1,5 @@
 import bpy, os
-from math import sin, cos, pi
 import random
-import numpy as np
 import json
 import sys
 
@@ -9,10 +7,10 @@ import sys
 Add scripts folder to Blender's Python interpreter and reload all scripts.
 http://web.purplefrog.com/~thoth/blender/python-cookbook/import-python.html
 """
-dir = os.path.dirname(bpy.data.filepath)
-if not dir in sys.path:
-    sys.path.append(dir)
-import importlib
+blender_path = os.path.dirname(bpy.data.filepath)
+if not blender_path in sys.path:
+    sys.path.append(blender_path)
+
 
 def render():
     """
@@ -20,71 +18,85 @@ def render():
     """
 
     scene = bpy.data.scenes['Scene']
-    camera_object = initialise_camera()
+    cameras = []
+    outpost_a_location = (-2.19561, -1.05119, 3.34377)
+    outpost_a_rotation = (0.976892, 0.0568127, -0.976659)
+    cameras.append(initialise_camera(outpost_a_location, outpost_a_rotation))
+
+    outpost_b_location = (10.1851, 5.63574, 3.34377)
+    outpost_b_rotation = (0.976892, 0.0568127, 2.15071)
+    cameras.append(initialise_camera(outpost_b_location, outpost_b_rotation))
+
     # TODO: Make custom robot object and insert it here instead of cube
     # The custom robot objects should created and placed inside the scene
     # and then their mesh names placed inside this array
-    mesh_objects = None # Create the 4 robot cubes through blender
-    batch_render(scene, camera_object, mesh_objects)
-
-def initialise_camera():
-    outpost_coordinate = (-2.19561, -1.05119, 3.34377)
-    # TODO: Figure out rotation properly and the units of rotation for camera_add()
-    camera_rotation = (41.61, -1.91622, -51)
-    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=outpost_coordinate, rotation=camera_rotation, scale=(1, 1, 1))
-    return bpy.data.objects['Camera']
+    mesh_objects = None  # Create the 4 robot cubes through blender
+    batch_render(scene, mesh_objects, cameras)
 
 
-def batch_render(scene, camera_object, mesh_objects):
+def initialise_camera(coordinates, rotation):
     """
-    Arguments:
+    Returns a wide-angle camera object with given coordinates and rotation that is set to the scene
+    """
+    outpost_coordinate = coordinates
+    bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=outpost_coordinate, rotation=rotation,
+                              scale=(1, 1, 1))
+    bpy.context.object.data.lens = 23
+    bpy.data.scenes['Scene'].camera = bpy.context.object
+    return bpy.context.object
 
-        scene: The scene from bpy.data.scenes
 
-        camera_object: The camera object from bpy.data.objects.camera
-
-        mesh_objects: List of robot objects that need to be randomly placed
-
-    """    
+def batch_render(scene, mesh_objects, camera_objects):
+    """
+    Sets up the render configurations, simulation configurations and then performs individual rendering
+    and label saving
+    """
 
     """ Environment Configurations 
     camera_frames = Number of frames for each individual simulation
     num_of_simulations = Number of simulations to run
     spawn_range = 3D range for the spawning of objects
     """
-    camera_frames = 30         
-    num_of_simulations = 3             
-    spawn_range = [       
+    camera_frames = 10
+    num_of_simulations = 3
+    spawn_range = [
         (0, 8),
         (0, 4),
         (0.2, 1.2)
     ]
     labels = []
 
+    # Cycles render engine parameters for optimal quality and performance
+    bpy.context.scene.cycles.device = 'GPU'
+    bpy.context.scene.cycles.samples = 4
+    bpy.context.scene.render.tile_x = 256
+    bpy.context.scene.render.tile_y = 256
+    bpy.context.scene.cycles.max_bounces = 4
 
     for i in range(0, num_of_simulations):
-        spawn_objects(scene, mesh_objects, spawn_range)
-        scene_labels = render(scene, camera_object, mesh_objects, camera_frames, file_prefix=i)
+        spawn_robots(mesh_objects, spawn_range)
+        scene_labels = render_helper(scene, mesh_objects, camera_objects, camera_frames, file_prefix=str(i))
         labels += scene_labels
 
-    save_labels_to_file(labels)   
+    save_labels_to_file(labels)
 
 
-def spawn_objects(scene, mesh_objects, spawn_range):
+def spawn_robots(mesh_objects, spawn_range):
     """
-    Randomly spawns the given objects in the given range
+    Randomly spawns the given robots in the given range
     """
-    for object in mesh_objects:
+    for robot in mesh_objects:
         # TODO: Ensure spawned objects do not clip the existing walls
-        object.location.x = random.uniform(spawn_range[0][0], spawn_range[0][1])
-        object.location.y = random.uniform(spawn_range[1][0], spawn_range[1][1])
-        object.location.z = random.uniform(spawn_range[2][0], spawn_range[2][1])
+        robot.location.x = random.uniform(spawn_range[0][0], spawn_range[0][1])
+        robot.location.y = random.uniform(spawn_range[1][0], spawn_range[1][1])
+        robot.location.z = random.uniform(spawn_range[2][0], spawn_range[2][1])
 
     print("spawn_objects runs 1/2")
 
-def render(scene, camera_object, mesh_objects, camera_frames, file_prefix="render"):
+
+def render_helper(scene, mesh_objects, camera_objects, camera_frames, file_prefix="render"):
     """
-    Renders the scene and returns a list of label data
+    Sets render configuration, renders the scene and returns a list of label data
     """
 
     # This stores the coordinates of the robots
@@ -93,42 +105,43 @@ def render(scene, camera_object, mesh_objects, camera_frames, file_prefix="rende
     # Rendering
     # https://blender.stackexchange.com/questions/1101/blender-rendering-automation-build-script
     for i in range(0, camera_frames + 1):
-        filename = 'Simulation{}-frame{}.png'.format(str(file_prefix), str(i))
-        bpy.context.scene.render.filepath = os.path.join(bpy.path.abspath("//renders/"), filename)
-
         # Changes keyframe to allow passage of time
         scene.frame_set(i)
 
-        # Cycles render engine parameters for optimal quality and performance
-        bpy.context.scene.cycles.device = 'GPU'
-        bpy.context.scene.cycles.samples = 4
-        bpy.context.scene.render.tile_x = 256
-        bpy.context.scene.render.tile_y = 256
-        bpy.context.scene.cycles.max_bounces = 4
-        bpy.ops.render.render(write_still=True)
+        # bpy.data.scenes[0].render.use_multiview = True
+        # bpy.data.scenes[0].render.views_format = 'MULTIVIEW'
+        for index, camera in enumerate(camera_objects):
+            filename = 'Simulation{}-frame{}-camera{}.png'.format(str(file_prefix), str(i), index)
+            bpy.context.scene.render.filepath = os.path.join(bpy.path.abspath("//renders/"), filename)
+            bpy.data.scenes['Scene'].camera = camera
+            bpy.ops.render.render(write_still=True)
 
-        # scene = bpy.data.scenes['Scene']
         label_entry = {
             'image': filename,
             'meshes': {}
         }
 
         # Get the placement coordinates of each robot
-        for object in mesh_objects:
-            label_entry['meshes'][object.name] = {
-                'x': object.location.x,
-                'y': object.location.y,
-                'z': object.location.z
+        for robot in mesh_objects:
+            label_entry['meshes'][robot.name] = {
+                'x': robot.location.x,
+                'y': robot.location.y,
+                'z': robot.location.z
             }
-            
+
         labels.append(label_entry)
     print("render runs 2/2")
     return labels
 
-    
+
 def save_labels_to_file(labels):
     """
     Saves the coordinates of the robot objects to a json for each individual frame
     """
-    with open(bpy.path.abspath("//renders/labels.json"), 'w+') as f:
-        json.dump(labels, f, sort_keys=True, indent=4, separators=(',', ': ')) 
+    with open(bpy.path.abspath("\\renders\\labels.json"), 'w+') as f:
+        json.dump(labels, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+    # For testing
+    render()
