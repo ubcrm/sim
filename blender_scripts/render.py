92af174cb6133896f7e9de71a4a62a27c5203fd2
Neelim Novo <neelim98@gmail.com>
2020-12-06 10:58:52 -0800

    Fixed random spawning of robots separately and randomised small movement generation

--- blender_scripts/render.py
+++ blender_scripts/render.py
@@ -6,6 +6,7 @@ import json
 import sys
 import time
 from mathutils.bvhtree import BVHTree
+from typing import List
 
 output_path = os.path.join(os.getcwd(), 'renders')
 if not os.path.isdir(output_path):
@@ -26,17 +27,19 @@ render_configs = {
     ],
     'camera_focal_length': 23,
     'robot_spawn_range': [
-        (0, 8),
-        (0, 4),
-        (0.2, 1.2)
+        (0.5, 7.5),
+        (0.4, 4),
+        (0.010, 0.011)
     ],
-    'num_of_simulations': 3,
-    'frames_per_simulation': 3,
+    'num_of_simulations': 2,
+    'frames_per_simulation': 50,
     'GPU_configs': {
         'device': 'GPU',
         'samples': 4,
         'tile_size': 256,
-        'max_bounces': 4
+        'max_bounces': 4,
+        'adaptive_sampling': True,
+        'enable_caustics': False
     }
 }
 
@@ -58,10 +61,9 @@ def render():
     outpost_b_rotation = render_configs['cameras'][1]['rotation']
     cameras.append(initialise_camera(outpost_b_location, outpost_b_rotation))
 
-    # TODO: Make custom robot object and insert it here instead of cube
     # The custom robot objects should created and placed inside the scene
-    # and then their mesh names placed inside this array
-    mesh_objects = ['r1_base', 'r2_base', 'r3_base', 'r4_base']  # Create the 4 robot cubes through blender
+    # and then their object names placed inside this array
+    mesh_objects = ['r1_base', 'r2_base', 'r3_base', 'r4_base']
     batch_render(scene, mesh_objects, cameras)
     print(f'Render finished in {time.time() - start_time}!')
 
@@ -103,42 +105,70 @@ def batch_render(scene, mesh_objects, camera_objects):
 
     for i in range(0, num_of_simulations):
         spawn_robots(mesh_objects, spawn_range)
-        scene_labels = render_helper(scene, mesh_objects, camera_objects, camera_frames, file_prefix=str(i))
-        labels += scene_labels
+        setup_animation_paths(mesh_objects)
+        simulation_labels = render_helper(scene, mesh_objects, camera_objects, camera_frames, simulation_number=str(i))
+        labels.append(simulation_labels)
 
     save_labels_to_file(labels)
 
 
-def spawn_robots(mesh_objects, spawn_range):
+def setup_animation_paths(mesh_objects):
+    scene = bpy.data.scenes[0]
+    x_transitions = [-0.5, 0.5]
+    y_transitions = [-0.25, 0.25]
+
+    for frame in range(0, 100, 50):
+        scene.frame_set(frame)
+        for robot in mesh_objects:
+            mesh = scene.objects[robot]
+            if frame == 0:
+                mesh.keyframe_insert(data_path='location')
+            else:
+                mesh.location.x = (mesh.location.x + x_transitions[random.randint(0, 1)])
+                mesh.location.y = (mesh.location.y + y_transitions[random.randint(0, 1)])
+                mesh.keyframe_insert(data_path='location')
+
+
+def spawn_robots(robot_objects, spawn_range):
     """
     Randomly spawns the given robots in the given range
+    Helper for batch_render()
     """
-
-    for robot in mesh_objects:
+    # Prepare list of environment blocks the robots should not overlap with
+    block_list = [f'block_B{i}_obj' for i in range(1, 10)]
+    block_list.append('wall_obj')
+    scene = bpy.data.scenes[0]
+    for robot in robot_objects:
         never_ran = True
-        robot_list = []
-        while never_ran or robot_intersects(robot.name):
-            robot.location.x = random.uniform(spawn_range[0][0], spawn_range[0][1])
-            robot.location.y = random.uniform(spawn_range[1][0], spawn_range[1][1])
-            robot.location.z = random.uniform(spawn_range[2][0], spawn_range[2][1])
+        while never_ran or robot_intersects(robot, block_list):
+            # Get the object and place it
+            mesh = scene.objects[robot]
+            mesh.location.x = random.uniform(spawn_range[0][0], spawn_range[0][1])
+            mesh.location.y = random.uniform(spawn_range[1][0], spawn_range[1][1])
+            mesh.location.z = random.uniform(spawn_range[2][0], spawn_range[2][1])
+            # Update object data
+            layer = bpy.context.view_layer
+            layer.update()
+            # print(f'Tried to insert robot {robot} at {mesh.location.x}, {mesh.location.y}, {mesh.location.z}')
             never_ran = False
+        # Once a robot has been successfully spawned, it should not intersect with newly spawned robots
+        block_list.append(robot)
 
-        robot_list.append(robot)
-
-    print("Robots randomly spawned")
+    print("Robots succesfully spawned")
 
 
-def robot_intersects(robot_object: str):
+def robot_intersects(robot_object: str, block_list: List[str]):
     """
-    Returns true if given object overlaps with any existing mesh, false otherwise
+    Returns true if given robot_object overlaps with any block mesh, false otherwise
 
     Helper function for spawn_robots()
 
     :param robot_object: name of the object to check for mesh overlap
     :type robot_object: str
+    :param block_list: string list of all the block obstacles in the environment
+    :type block_list: List[str]
     """
 
-    intersections = []
     scene = bpy.data.scenes[0]
 
     # Create a copy of the object for matrix transformation
@@ -146,32 +176,38 @@ def robot_intersects(robot_object: str):
     copy_mesh.from_mesh(scene.objects[robot_object].data)
     copy_mesh.transform(scene.objects[robot_object].matrix_world)
     robot_bvtree = BVHTree.FromBMesh(copy_mesh)
-
-    # Check if spawned robot overlaps with existing mesh
-    for mesh in bpy.data.meshes:
+    # Check if spawned robot overlaps with existing meshes
+    for block in block_list:
         comparison_mesh = bmesh.new()
-        comparison_mesh.from_mesh(scene.objects[mesh].data)
-        comparison_mesh.transform(scene.objects[mesh].matrix_world)
+        comparison_mesh.from_mesh(scene.objects[block].data)
+        comparison_mesh.transform(scene.objects[block].matrix_world)
         comparison_bvtree = BVHTree.FromBMesh(comparison_mesh)
-        intersections.append(comparison_bvtree.overlap(robot_bvtree))
-        if intersections:
+        intersections = comparison_bvtree.overlap(robot_bvtree)
+        if len(intersections) > 0:
+            print(f'{block} intersects with {robot_object} at {len(intersections)} points, respawning...')
             return True
-
     return False
 
 
-def render_helper(scene, robot_meshes, camera_objects, camera_frames, file_prefix="render"):
+def render_helper(scene, robot_names, camera_objects, camera_frames, simulation_number='render'):
     """
-    Sets render configuration, renders the scene and returns a list of label data
+    Sets render configuration, renders the scene and returns a the individual simulation data in a dict
     """
 
     # This stores information about each robot, and its coordinates per frame
-    labels = []
-    for robot in robot_meshes:
-        labels.append({
-            'robot_name': str(robot.name),
-            'robot_coordinates': []
-        })
+    sim_no = f'simulation_number{simulation_number}'
+    simulation_log = {
+        sim_no: {
+            f'{robot_names[0]}': [],
+            f'{robot_names[1]}': [],
+            f'{robot_names[2]}': [],
+            f'{robot_names[3]}': []
+        }}
+    # for robot in robot_names:
+    #     simulation_log[sim_no].append({
+    #         f'robot_{robot}': []
+    #     })
+    # print(simulation_log)
 
     # Rendering
     # https://blender.stackexchange.com/questions/1101/blender-rendering-automation-build-script
@@ -179,23 +215,26 @@ def render_helper(scene, robot_meshes, camera_objects, camera_frames, file_prefi
         # Changes keyframe to allow passage of time
         scene.frame_set(frame)
         for index, camera in enumerate(camera_objects):
-            filename = 'Simulation{}-frame{}-camera{}.png'.format(str(file_prefix), str(frame), index)
+            filename = f'Simulation{simulation_number}-frame{str(frame)}-camera{index}.png'
             camera_path = os.path.join(output_path, f'camera{index + 1}')
             bpy.context.scene.render.filepath = os.path.join(camera_path, filename)
             bpy.data.scenes[0].camera = camera
             bpy.ops.render.render(write_still=True)
 
         # Get the placement coordinates of each robot
-        for index, robot in enumerate(robot_meshes):
-            coordinate_entry = {
-                'frame': frame,
-                'x': robot.location.x,
-                'y': robot.location.y,
-                'z': robot.location.z
+        for index, robot in enumerate(robot_names):
+            mesh = bpy.context.scene.objects[robot]
+            frame_position = {
+                "frame_no": frame,
+                "coordinates": {
+                    "x": mesh.location.x,
+                    "y": mesh.location.y,
+                    "z": mesh.location.z
+                }
             }
-            labels[index]['robot_coordinates'].append(coordinate_entry)
+            simulation_log[sim_no][robot].append(frame_position)
 
-    return labels
+    return simulation_log
 
 
 def save_labels_to_file(labels):
